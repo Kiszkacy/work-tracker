40a26c2dec7ad131aef986b2b91c7e2e43d78547
Kiszkacy <karolkisz22@gmail.com>
2025-03-04 20:29:47 +0100

    fix start and end commands

--- work_tracker/command/commands/end.py
+++ work_tracker/command/commands/end.py
@@ -2,8 +2,8 @@ import datetime
 
 from work_tracker.command.command_handler import CommandHandlerResult, CommandHandler
 from work_tracker.command.common import CommandArgument, TimeArgument
-from work_tracker.common import Date, ReadonlyAppState, Mode, Time, DayData
-from work_tracker.error import CommandErrorInvalidArgumentCount, CommandErrorInvalidMode
+from work_tracker.common import Date, ReadonlyAppState, Mode, Time, DayData, find_first_not_fulfilling
+from work_tracker.error import CommandErrorInvalidArgumentCount, CommandErrorInvalidMode, CommandErrorInvalidDate
 
 
 class EndHandler(CommandHandler):
@@ -15,13 +15,7 @@ class EndHandler(CommandHandler):
                 case _:
                     return CommandHandlerResult(undoable=False, error=CommandErrorInvalidMode(self.command_name, mode=state.mode))
 
-            time_now: datetime.datetime = datetime.datetime.now()
-            day: DayData = self.data.day[state.active_date]
-            day.work_end = Time(
-                minutes_since_midnight=time_now.hour * 60 + time_now.minute
-            )
-            if day.work_start is not None:
-                day.minutes_at_work = day.work_end.minutes_since_midnight - day.work_start.minutes_since_midnight
+            self._handle_day(state.active_date)
             return CommandHandlerResult(undoable=True)
         elif date_count == 0 and argument_count == 1: # TODO handle if end time is before start time
             match state.mode:
@@ -30,13 +24,41 @@ class EndHandler(CommandHandler):
                 case _:
                     return CommandHandlerResult(undoable=False, error=CommandErrorInvalidMode(self.command_name, mode=state.mode))
 
-            time: TimeArgument = arguments[0]
-            day: DayData = self.data.day[state.active_date]
-            day.work_end = Time(
+            self._handle_day(state.active_date, arguments[0])
+            return CommandHandlerResult(undoable=True)
+        elif date_count != 0 and argument_count == 0:
+            if invalid_date := find_first_not_fulfilling(dates, lambda date: date.is_day_date()):
+                return CommandHandlerResult(undoable=False, error=CommandErrorInvalidDate(self.command_name, received_date=invalid_date))
+            for date in dates:
+                self._handle_day(date.fill_with(state.active_date))
+            return CommandHandlerResult(undoable=True)
+        elif date_count != 0 and argument_count == 1:
+            if invalid_date := find_first_not_fulfilling(dates, lambda date: date.is_day_date()):
+                return CommandHandlerResult(undoable=False, error=CommandErrorInvalidDate(self.command_name, received_date=invalid_date))
+            for date in dates:
+                self._handle_day(date.fill_with(state.active_date), arguments[0])
+            return CommandHandlerResult(undoable=True)
+        else:  # argument_count > 1
+            return CommandHandlerResult(undoable=False, error=CommandErrorInvalidArgumentCount(self.command_name, received_argument_count=argument_count))
+
+    def _handle_day(self, date: Date, time: TimeArgument | None = None):
+        date = date.fill_with_today().to_day_date()
+        day: DayData = self.data.day[date]
+        if time is not None:
+            time_end: Time = Time(
                 minutes_since_midnight=time.minutes
             )
-            if day.work_start is not None:
-                day.minutes_at_work = day.work_end.minutes_since_midnight - day.work_start.minutes_since_midnight
-            return CommandHandlerResult(undoable=True)
-        else: # argument_count > 1
-            return CommandHandlerResult(undoable=False, error=CommandErrorInvalidArgumentCount(self.command_name, received_argument_count=argument_count))
+        else:
+            time_now: datetime.datetime = datetime.datetime.now()
+            time_end: Time = Time(
+                minutes_since_midnight=time_now.hour * 60 + time_now.minute
+            )
+        day.work_end = time_end
+
+        if day.work_start is not None and day.work_start.minutes_since_midnight > day.work_end.minutes_since_midnight:
+            day.work_start = time_end
+            day.minutes_at_work = 0
+        elif day.work_start is not None:
+            day.minutes_at_work = day.work_end.minutes_since_midnight - day.work_start.minutes_since_midnight
+
+        return CommandHandlerResult(undoable=True)
