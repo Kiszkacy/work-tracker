fb4a99c25b9a26949b8974e9e0335cbf0b81e5c1
Kiszkacy <karolkisz22@gmail.com>
2026-03-04 19:21:58 +0100

    fix autocompletion on backspace click

--- work_tracker/text/input_output_handler.py
+++ work_tracker/text/input_output_handler.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import colorama
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
+from prompt_toolkit.key_binding import KeyBindings
 
 from work_tracker.common import get_data_path
 from work_tracker.config import Config
@@ -14,9 +15,29 @@ class InputOutputHandler:
     def __init__(self):
         colorama.init()
         self._writer: TextWriter = TextWriter()
-        self.session: PromptSession = PromptSession(history=FileHistory(get_data_path().joinpath("cmd-history")))
+        self.session: PromptSession = self._initialize_session()
         self.command_completer: InputCommandCompleter = InputCommandCompleter()
         self._truncate_history()
+
+    def _initialize_session(self) -> PromptSession:
+        keybinds: KeyBindings = KeyBindings()
+
+        @keybinds.add('backspace')
+        def _(event):
+            buffer = event.app.current_buffer
+            if buffer.cursor_position > 0:
+                buffer.delete_before_cursor()
+
+            if self.command_completer.active and buffer.text:
+                buffer.start_completion(select_first=False)
+            elif not buffer.text:
+                buffer.cancel_completion()
+
+        return PromptSession(
+            history=FileHistory(get_data_path().joinpath("cmd-history")),
+            key_bindings=keybinds,
+            complete_while_typing=True
+        )
 
     @staticmethod
     def _truncate_history():
